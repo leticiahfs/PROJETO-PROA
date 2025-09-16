9f39b86268683dca76e3ce4863598bd77f367a48
Letícia Figueiredo <leticiahfs17@gmail.com>
2025-09-15 22:34:06 -0300

    initial commit

---
+++ assets/script.js
@@ -0,0 +1,4 @@
+
+//criar validação de fomulários (para a pessoa não conseguir enviar o cadastro/login vazio)
+//mensagem de boas-vindas após o login/cadastro
+//pesquisa nos artigos - filtro simples com JS
